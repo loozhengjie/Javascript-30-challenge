9089dffd2730b7eecf362f8685c1dafba9b8a380
loozhengjie <loozhengjie555@gmail.com>
2025-04-19 07:54:26 +0800

    starting to refer to video, implemented skip buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,16 +8,28 @@ const videoPlayer = document.querySelector(".viewer");
 function setVideoPlayStatus (e){
     if (videoPlayer.paused) {
         videoPlayer.play();
-        playButton.textContent = "❚❚";
+        // Update, its ok to update the text context on toggle play / pause on clicked, but its better to do it on actual video play / pause
+        // Cause there might be plugins where it will just paused the video, then this function will not get called and the text content will not get updated
+        // playButton.textContent = "❚❚";
     } else {
         videoPlayer.pause();
+    }
+}
+
+function updateButton(){
+    if (videoPlayer.paused) {
         playButton.textContent = "►";
+    } else {
+        playButton.textContent = "❚❚";
     }
 }
 
 // When the play button and the video player is preessed, call the function to modify the status
 playButton.addEventListener("click", setVideoPlayStatus);
 videoPlayer.addEventListener("click", setVideoPlayStatus);
+videoPlayer.addEventListener("play", updateButton);
+videoPlayer.addEventListener("pause", updateButton);
+
 
 // Get the progress bar
 const progressBar = document.querySelector(".progress__filled");
@@ -29,3 +41,17 @@ videoPlayer.addEventListener("timeupdate", function(){
     // Update the progress bar with the progress, dont forget the "%"
     progressBar.style.flexBasis = `${progress}%`;
 });
+
+const skipButtons = document.querySelectorAll("[data-skip]"); 
+
+function skip(){
+    console.log(this.dataset);
+    videoPlayer.currentTime += parseFloat(this.dataset.skip); // This.dataset.skip is a string, so need to convert it to true number
+}
+
+skipButtons.forEach(skipButton => skipButton.addEventListener("click", skip));
+
+
+// Video stop at 13:05
+
+const ranges = document.querySelectorAll(".player-slider"); 
