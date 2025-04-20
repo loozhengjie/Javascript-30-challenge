dfd2e19899818fa2dcfa5e32ee6de41fb71d84ba
loozhengjie <loozhengjie555@gmail.com>
2025-04-20 19:26:08 +0800

    Course 11 done

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -32,20 +32,20 @@ videoPlayer.addEventListener("pause", updateButton);
 
 
 // Get the progress bar
-const progressBar = document.querySelector(".progress__filled");
+const progressBarFill = document.querySelector(".progress__filled");
 
 // When the video player is playing
 videoPlayer.addEventListener("timeupdate", function(){
     // Use the current video player time / video player duration to get the progress, then *100 to get the percentage
     const progress = (this.currentTime / this.duration) * 100;
     // Update the progress bar with the progress, dont forget the "%"
-    progressBar.style.flexBasis = `${progress}%`;
+    progressBarFill.style.flexBasis = `${progress}%`;
 });
 
 const skipButtons = document.querySelectorAll("[data-skip]"); 
 
 function skip(){
-    console.log(this.dataset);
+    // console.log(this.dataset);
     videoPlayer.currentTime += parseFloat(this.dataset.skip); // This.dataset.skip is a string, so need to convert it to true number
 }
 
@@ -54,4 +54,50 @@ skipButtons.forEach(skipButton => skipButton.addEventListener("click", skip));
 
 // Video stop at 13:05
 
-const ranges = document.querySelectorAll(".player-slider"); 
+const ranges = document.querySelectorAll(".player__slider"); 
+
+function handleRangeUpdate(){
+    // This one gets an element and its attribute, then change that attribute value
+    videoPlayer[this.name] = this.value;
+}
+
+ranges.forEach(range=>range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range=>range.addEventListener("mousemove", handleRangeUpdate));
+
+let isMouseClicked = false;
+const progressBar = document.querySelector(".progress"); 
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progressBar.offsetWidth)* videoPlayer.duration;
+    videoPlayer.currentTime = scrubTime;
+}
+
+
+progressBar.addEventListener("mouseup", ()=> isMouseClicked = false);
+progressBar.addEventListener("mousedown", ()=> isMouseClicked = true);
+
+progressBar.addEventListener("click", scrub);
+// We can use "&&" as "short-circuit evaluation", so if the left side is truthy, then it will run the right hand side
+progressBar.addEventListener("mousemove", (e)=> isMouseClicked && scrub(e));
+
+const playerContainer = document.querySelector(".player");
+
+// Full screen event test
+const fullscreenButton = document.querySelector(".fullscreen"); 
+fullscreenButton.addEventListener("click", function(){
+    // Only the document can check if an element is fullscreen, and control it, think about it as the central hub
+    if (!document.fullscreenElement) {
+        // For requesting, the element has to call the document to control on its behalf
+        playerContainer.requestFullscreen();
+    }
+    else{
+        // Of course, the document controls the handling
+        document.exitFullscreen();
+    }
+})
+
+// Find this js fullscreen api weird tho haha, why not just make every function from zoom in and zoom out from document, 
+// instead of full screen from element and exit full screen from document,
+// better if document.toggleFullscreen(element);
+// just a thought haha
+
