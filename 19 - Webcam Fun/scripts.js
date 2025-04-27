e78d64cf375e667f40e169ddf25f0bdcea5af3b2
loozhengjie <loozhengjie555@gmail.com>
2025-04-27 22:01:54 +0800

    course 19 - thats alot to take in

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,51 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+// video stop at 2:49
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(localMediaStream =>{
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+    }).catch(err=> console.error("No webcam"))
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    console.log(width, height);
+
+    // Every 16 ms, update the video data to canvas
+    return setInterval(()=>{
+        ctx.drawImage(video, 0,0 ,width, height);
+    }, 16)
+}
+
+function takePhoto(){
+    // Played the sound
+    snap.currentTime = 0;
+    snap.play();
+    
+    // Take the data out of canvas
+    // It will return text base rendition of the picture
+    const data =  canvas.toDataURL("image / jpeg");
+    console.log(data);
+
+    const link = document.createElement("a");
+    link.href = data;
+    link.setAttribute("download", "handsome");
+    link.innerHTML = `<img src="${data}" alt="Handsome Man">`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
+
+// Video stop at 18:25
