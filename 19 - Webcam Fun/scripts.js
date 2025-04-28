096343266daed609a3afd5e41ea2819a8330e713
loozhengjie <loozhengjie555@gmail.com>
2025-04-28 21:31:24 +0800

    course 19 done, r3move npm modules for safety concerns

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,6 +24,13 @@ function paintToCanvas(){
     // Every 16 ms, update the video data to canvas
     return setInterval(()=>{
         ctx.drawImage(video, 0,0 ,width, height);
+        // Take pixels out
+        let pixels =  ctx.getImageData(0, 0, width, height);
+        // Mess with them
+        pixels = greenScreen(pixels);
+        // ctx.globalAlpha = 0.1;
+        // Put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16)
 }
 
@@ -42,6 +49,58 @@ function takePhoto(){
     link.setAttribute("download", "handsome");
     link.innerHTML = `<img src="${data}" alt="Handsome Man">`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels){
+    for(let i = 0; i < pixels.data.length; i +=4){
+        // r
+        pixels.data[i + 0] = pixels.data[i + 0] + 100;
+        // g
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        // b
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+
+    return pixels;
+}
+
+function rgbSplit(pixels){
+    for(let i = 0; i < pixels.data.length; i +=4){
+        // r
+        pixels.data[i - 150] = pixels.data[i + 0];
+        // g
+        pixels.data[i + 100] = pixels.data[i + 1];
+        // b
+        pixels.data[i + 150] = pixels.data[i + 2];
+    }
+    return pixels;
+}
+
+function greenScreen(pixels){
+    const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 // getVideo();
